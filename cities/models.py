f84a03ecc64f987e6c880579abdc04b8abbe162a
lautarianoo <neonchick1>
2021-03-24 11:20:13 +0500

    2 запуск коммит

---
+++ cities/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+        ordering = ['name']
